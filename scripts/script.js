7b4a90c0142cd524a72d4725ee3f31da7163e998
Farhan Rana <rana.tdm@gmail.com>
2025-11-09 10:27:27 +0500

    Add menu toggle functionality to navigation

--- scripts/script.js
+++ scripts/script.js
@@ -53,3 +53,10 @@ function filterProducts() {
     }
   });
 }
+
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navMenu = document.querySelector('nav ul');
+
+  menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
